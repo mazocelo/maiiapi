eaa657c463314302ccb8b1ee90dce6945fb73b7e
Marcelo Mazarem Correa <mazarem91@gmail.com>
2021-05-22 15:52:15 -0300

    first test

--- routes/ranking.js
+++ routes/ranking.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("memoriadelixo").collection("ranking");
+        res.send(collection)
+            //client.close();
+    });
+
+
+});
+router.post('/', function(req, res, next) {
+
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("memoriadelixo").collection("ranking");
+        collection.insertOne(req.body, function(err, resp) {
+            if (err) {
+                console.log('erro', resp, err)
+                throw err
+            }
+
+            console.log("novo usuario inserido");
+            res.send('ok')
+        })
+
+    })
+
 });
 
 module.exports = router;
